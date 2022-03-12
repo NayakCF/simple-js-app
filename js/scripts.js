c518ecb7868010aa263c9eef50a5d88fceda2515
NayakCF <vishwamusic@gmail.com>
2022-03-12 09:48:25 +0100

    SyntaxErrorsFixed/JS

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,5 @@
-let name ='polemonlist';
-
-let pokemonList= [{
-  name:'Bulbasaur',
-  height: 7,
-  types:['grass','poison'],
-},
-
-{
-  name:'Charizard',
-  height: 5.07,
-  type: ['fire', 'flying']
-  },
-  
-{
-  name: 'Pikachu',
-  height: 1.04,
-  type: ['electric']
-  },
-  ];
+let pokemonList= [
+    {name:'Bulbasaur', height: 7,types:['grass','poison']},
+    {name:'Charizard', height: 5.07,type: ['fire', 'flying']},
+    {name:'Pikachu',height: 1.04,type: ['electric']},
+   ];
