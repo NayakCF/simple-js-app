669341b308e98976c9bb9e29931451db539dc85b
NayakCF <vishwamusic@gmail.com>
2022-03-15 16:02:15 +0100

    First ForLoop_JS
    Added the For loop and If_elseif_else. Used documentWrite within loop.

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,19 @@
-let pokemonList= [
-    {name:'Bulbasaur', height: 7,type:['grass','poison']},
-    {name:'Charizard', height: 5.07,type: ['fire', 'flying']},
-    {name:'Pikachu',height: 1.04,type: ['electric']},
-   ];
+let pokemon = [
+    { name: 'Bulbasaur', height: 1.5, types: ['grass', 'poison'] },
+    { name: 'Charizard', height: 1, type: ['fire', 'flying'] },
+    { name: 'Pikachu', height: 0.5, type: ['electric'] }
+    ];
+    for (let i=0; i < pokemon.length; i++){
+    if (pokemon[i].height >1.4) {
+    console.log(pokemon[i].name+"Wow, that is big pokeman!");
+      { document.write("Bulbasaur "+"Height 1.5" )
+          }
+    }else if (pokemon[i].height <1){
+    console.log(pokemon[i].name+"this is average pokemon");
+      { document.write("Charizard "+"Height 1" )
+          }
+    } else { console.log(pokemon[i].name+"this is a small pokemon"); 
+            { document.write("Pikachu "+"Height 0.5" )
+          }
+    }
+    }
