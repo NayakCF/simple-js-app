251ca7903ec4dfbde98ac21e26fdb189a4a5efa5
NayakCF <vishwamusic@gmail.com>
2022-03-19 21:43:06 +0100

    TaskPart2/getall/add/pokemonList
    used the IIFE protocol within the pokemonrepository and added the new person to the list.
    getAll function to reflect the person details to view on console.

--- js/scripts.js
+++ js/scripts.js
@@ -8,4 +8,21 @@ let pokemonList = [
     pokemonList.forEach(function(pokemonList){
       console.log(pokemonList.name +' is a person with height of ' + pokemonList.height + ' ' + pokemonList.type + ' are the charectoristics type.')
     });
-
+    // created the repository and added Eevee to the array list using add property
+    let pokemonRepository = (function () {
+      let pokemonList = [
+        { name: 'Bulbasaur', height: 1.5, type: ['grass', 'poison'] },
+        { name: 'Charizard', height: 1, type: ['fire', 'flying'] },
+        { name: 'Pikachu', height: 0.5, type: ['electric'] }]; 
+      return {
+        add: function(pokemon) {
+          pokemonList.push(pokemon);
+        },
+        getAll: function() {
+          return pokemonList;
+        }
+      };
+    })();
+    pokemonRepository.add({ name: 'Eevee', height:30.5, type:['Normal'] });
+    console.log(pokemonRepository.getAll());  
+  
