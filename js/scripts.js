05a59e155d38f03dedbcc80d4a4bb8ce4ca69c02
NayakCF <vishwamusic@gmail.com>
2022-03-19 21:00:45 +0100

    forEachLoop/PokemonList
    use of forEach loop to fetch the data in console from the PokemonList array.

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,11 @@
-let pokemon = [
+let pokemonList = [
     { name: 'Bulbasaur', height: 1.5, types: ['grass', 'poison'] },
     { name: 'Charizard', height: 1, type: ['fire', 'flying'] },
     { name: 'Pikachu', height: 0.5, type: ['electric'] }
     ];
-    for (let i=0; i < pokemon.length; i++){
-    if (pokemon[i].height >1.4) {
-    console.log(pokemon[i].name+"Wow, that is big pokeman!");
-      { document.write("Bulbasaur "+"Height 1.5" )
-          }
-    }else if (pokemon[i].height <1){
-    console.log(pokemon[i].name+"this is average pokemon");
-      { document.write("Charizard "+"Height 1" )
-          }
-    } else { console.log(pokemon[i].name+"this is a small pokemon"); 
-            { document.write("Pikachu "+"Height 0.5" )
-          }
-    }
-    }
+    // forEach () being used to fetch the information from the pokemonList array
+
+    pokemonList.forEach(function(pokemonList){
+      console.log(pokemonList.name +' is a person with height of ' + pokemonList.height + ' ' + pokemonList.type + ' are the charectoristics.')
+    });
+
