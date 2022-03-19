8ed5f555cd5e0593cd119f50e5461c4c3b1dd13b
NayakCF <vishwamusic@gmail.com>
2022-03-19 21:08:02 +0100

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,11 @@
 let pokemonList = [
-    { name: 'Bulbasaur', height: 1.5, types: ['grass', 'poison'] },
+    { name: 'Bulbasaur', height: 1.5, type: ['grass', 'poison'] },
     { name: 'Charizard', height: 1, type: ['fire', 'flying'] },
     { name: 'Pikachu', height: 0.5, type: ['electric'] }
     ];
     // forEach () being used to fetch the information from the pokemonList array
 
     pokemonList.forEach(function(pokemonList){
-      console.log(pokemonList.name +' is a person with height of ' + pokemonList.height + ' ' + pokemonList.type + ' are the charectoristics.')
+      console.log(pokemonList.name +' is a person with height of ' + pokemonList.height + ' ' + pokemonList.type + ' are the charectoristics type.')
     });
 
