cc7485ad52449009b4ec59ac94924018e2772aaa
NayakCF <vishwamusic@gmail.com>
2022-03-21 08:56:38 +0100

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -4,16 +4,16 @@ let pokemonList = [
     { name: 'Pikachu', height: 0.5, type: ['electric'] }
     ];
     // forEach () being used to fetch the information from the pokemonList array
-
-    pokemonList.forEach(function(pokemonList){
-      console.log(pokemonList.name +' is a person with height of ' + pokemonList.height + ' ' + pokemonList.type + ' are the charectoristics type.')
-    });
+    
+      pokemonList.forEach(function(pokemonList){
+        console.log(pokemonList.name +' is a person with height of ' + pokemonList.height + ' ' + pokemonList.type + ' are the charectoristics type.')
+        });
+   
+   
     // created the repository and added Eevee to the array list using add property
     let pokemonRepository = (function () {
-      let pokemonList = [
-        { name: 'Bulbasaur', height: 1.5, type: ['grass', 'poison'] },
-        { name: 'Charizard', height: 1, type: ['fire', 'flying'] },
-        { name: 'Pikachu', height: 0.5, type: ['electric'] }]; 
+      let pokemonList = []
+    
       return {
         add: function(pokemon) {
           pokemonList.push(pokemon);
