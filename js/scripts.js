c51e83f57d3c31ffde2de0b070f7dd05b3c077b9
NayakCF <vishwamusic@gmail.com>
2022-03-22 17:37:09 +0100

    DOM/JS/Interact with Html
    Created the UL in Html, added the object to array, created button and using innerText add the pokemon names.
    Added the style to CSS file to make things look nicer.

--- js/scripts.js
+++ js/scripts.js
@@ -1,28 +1,60 @@
-let pokemonList = [
-    { name: 'Bulbasaur', height: 1.5, type: ['grass', 'poison'] },
-    { name: 'Charizard', height: 1, type: ['fire', 'flying'] },
-    { name: 'Pikachu', height: 0.5, type: ['electric'] }
+let pokemonRepository = (function () {
+    let repository = [
+      {
+        name: "Bulbasaur",
+        height: 0.7,
+        types: ["grass", "poison"],
+      },
+      {
+        name: "Charizard",
+        height: 1.7,
+        types: ["fire", "flying"],
+      },
+      {
+        name: "Squirtle",
+        height: 1,
+        types: ["water"],
+      },
     ];
-    // forEach () being used to fetch the information from the pokemonList array
-    
-      pokemonList.forEach(function(pokemonList){
-        console.log(pokemonList.name +' is a person with height of ' + pokemonList.height + ' ' + pokemonList.type + ' are the charectoristics type.')
-        });
-   
-   
-    // created the repository and added Eevee to the array list using add property
-    let pokemonRepository = (function () {
-      let pokemonList = []
-    
-      return {
-        add: function(pokemon) {
-          pokemonList.push(pokemon);
-        },
-        getAll: function() {
-          return pokemonList;
-        }
-      };
-    })();
-    pokemonRepository.add({ name: 'Eevee', height:30.5, type:['Normal'] });
-    console.log(pokemonRepository.getAll());  
   
+    function add(pokemon) {
+      if (
+        typeof pokemon === "object" &&
+        "name" in pokemon &&
+        "height" in pokemon &&
+        "types" in pokemon
+      ) {
+        repository.push(pokemon);
+      } else {
+        console.log("pokemon is not correct");
+      }
+    }
+    function getAll() {
+      return repository;
+    }
+    function addListItem(pokemon){
+      //Traversing using DOM in JS and access the pokemonlist in index.html  // 
+      let pokemonList = document.querySelector(".pokemon-list");
+      let listpokemon = document.createElement("li");
+      let button = document.createElement("button");
+      button.innerText = pokemon.name;
+      button.classList.add("button-class");
+      listpokemon.appendChild(button);
+      pokemonList.appendChild(listpokemon);
+    }
+    return {
+      add: add,
+      getAll: getAll,
+      addListItem: addListItem
+    };
+  })();
+  // Add the object to existing array and ussing getall() to check the same in console//
+  pokemonRepository.add({ name: "Eevee", height: 1, types: ["Run Away","Adaptability"] });
+  
+  console.log(pokemonRepository.getAll());
+
+  
+  pokemonRepository.getAll().forEach(function(pokemon){
+  pokemonRepository.addListItem(pokemon);
+
+  });
